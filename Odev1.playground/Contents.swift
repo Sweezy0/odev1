18a7568b886f13c1d8328a0397a6e62f61c6320a
Furkan Şentürk <119443953+Sweezy0@users.noreply.github.com>
2024-05-16 13:53:02 +0300

    Add files via upload

---
+++ Odev1.playground/Contents.swift
@@ -0,0 +1,142 @@
+import UIKit
+
+var ilce: String = "Etimesgut"
+print("İLÇE : \(ilce)")
+
+var kita: String = "Ortadoğu"
+print("KITA : \(kita)")
+
+var faks: Int = 12345678
+print("FAKS : \(faks)")
+
+var postaKodu: Int = 456789134
+print("POSTA KODU : \(postaKodu)")
+
+var instaAdresi: String = "user12345"
+print("İNSTA ADRESİ : \(instaAdresi)")
+
+var calistiginBolum: String = "Yazılım"
+print("ÇALIŞTIĞIN BÖLÜM : \(calistiginBolum)")
+
+var urunMiktari: Int = 100
+print("ÜRÜN MİKTARI : \(urunMiktari)")
+
+var musteriSoyadi: String = "Kahveci"
+print("MÜŞTERİ SOYADI : \(musteriSoyadi)")
+
+var odemeMiktari: Int = 300
+print("ÖDEME MİKTARI : \(odemeMiktari)")
+
+var dogumTarihi: String = "08.09.2001"
+print("DOĞUM TARİHİ : \(dogumTarihi)")
+
+var borc: Int = 1150
+print("BORÇ : \(borc)")
+
+var medeniHal: String = "Bekar"
+print("MEDENİ HAL : \(medeniHal)")
+
+var videoYorumu: String = "Etkileyici"
+print("VİDEO YORUMU : \(videoYorumu)")
+
+var odemeSaati: String = "23.59"
+print("ÖDEME SAATİ : \(odemeSaati)")
+
+var eftMiktari: String = "2500"
+print("EFT MİKTARI : \(eftMiktari)")
+
+var satilanMiktar: Int = 100
+print("SATILAN MİKTAR : \(satilanMiktar)")
+
+var telefonModeli: String = "Iphone 15 Pro"
+print("TELEFON MODELİ : \(telefonModeli)")
+
+var dergiAdi: String = "Medium"
+print("DERGİ ADI : \(dergiAdi)")
+
+var basimTarihi: String = "01.01.2024"
+print("BASIM TARİHİ : \(basimTarihi)")
+
+var zamMiktari: String = "%25"
+print("ZAM MİKTARI : \(zamMiktari)")
+
+var daireSayisi: Int = 45
+print("DAİRE SAYISI : \(daireSayisi)")
+
+var enlem: String = "45 Derece"
+print("ENLEM : \(enlem)")
+
+var boylam: String = "20 Derece"
+print("BOYLAM : \(boylam)")
+
+var yemekAdi: String = "Kebap"
+print("YEMEK ADI : \(yemekAdi)")
+
+var urunFiyati: Int = 250
+print("ÜRÜN FİYATI : \(urunFiyati)")
+
+var sirket: String = "Silikon Vadisi"
+print("ŞİRKET : \(sirket)")
+
+var videoAdi: String = "Football Skilss"
+print("VİDEO ADI : \(videoAdi)")
+
+var muzikSuresi: String = "3.45 Saniye"
+print("MÜZİK SÜRESİ : \(muzikSuresi)")
+
+var mekanPuani: Int = 5
+print("MEKAN PUANI : \(mekanPuani)")
+
+var dosyaAdi: String = "İK Dosyası"
+print("DOSYA ADI : \(dosyaAdi)")
+
+var resimFormati: String = "Düzenli"
+print("RESİM FORMATI : \(resimFormati)")
+
+var renk: String = "Mavi"
+print("RENK : \(renk)")
+
+var renkKodu: Double = 12.67
+print("RENK KODU : \(renkKodu)")
+
+var bilgisayarModeli: String = "Masaüstü"
+print("BİLGİSAYAR MODELİ : \(bilgisayarModeli)")
+
+var ekranBoyutu: String = "25 cm"
+print("EKRAN BOYUTU : \(ekranBoyutu)")
+
+var kullanimSuresi: String = "30 dk"
+print("KULLANIM SÜRESİ : \(kullanimSuresi)")
+
+var basinc: Double = 35.5
+print("BASINÇ : \(basinc)")
+
+var etkinlikGunu: String = "05.05.2024"
+print("ETKİNLİK GÜNÜ : \(etkinlikGunu)")
+
+var odemeGunu: String = "12.06.2024"
+print("ÖDEME GÜNÜ : \(odemeGunu)")
+
+var yolculukCikisSaati: Double = 12.45
+print("YOLCULUK ÇIKIŞ SAATİ : \(yolculukCikisSaati)")
+
+var mahalleAdi: String = "Atayurt Mahallesi"
+print("MAHALLE ADI : \(mahalleAdi)")
+
+var otobusAdi: String = "Kızılay Otobüsü"
+print("OTOBÜS ADI : \(otobusAdi)")
+
+var kullanilanDakika: String = "15 dk"
+print("KULLANILAN DAKİKA : \(kullanilanDakika)")
+
+var kargoTakipNo: Int = 76578314682164
+print("KARGO TAKİP NO : \(kargoTakipNo)")
+
+var kuponSuresi: String = "15 Gün"
+print("KUPON SÜRESİ : \(kuponSuresi)")
+
+var kuponKodu: String = "1asfNH89Öcnv"
+print("KUPON KODU : \(kuponKodu)")
+
+var faturaTarihi: String = "12.08.2024"
+print("FATURA TARİHİ : \(faturaTarihi)")
